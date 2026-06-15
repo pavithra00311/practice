3f818084610ff93db19018fee2f2f5478be470c5
pavithra <pavithraeraganaboyina@gmail.com>
2026-06-15 16:47:41 +0530

    Add files via upload

---
+++ op practice.py
@@ -0,0 +1,71 @@
+a=5
+b=3
+add=5+3
+print(add)
+print("subtract 10 from 25:", 25-10)
+print("multiply 4 by 6:",4*6)
+print("divide 20 by 4:",20/4)
+print(9 % 2)
+print(17 // 3)
+print(2 ** 5)
+print(2+4+6)
+print(10 * 0.5)
+print( 8 * 8)
+
+a=5
+b=5
+print(a == b)
+print(10 != 8)
+print(7 >3)
+print(4 < 9)
+print(12 >= 12)
+print(15 <= 10)
+a=10
+b=20
+print(a == b)
+print("apple == apple:", "apple" == "apple")
+x=5
+y=5
+print(x == y)
+score= 45
+print(score > 40)
+    
+result1 = 5 > 2 and 5 < 10
+print(f"is 5 > 2 and 5 < 10:,{result1}")
+result2 = 8 < 5 or 8 > 6
+print(f"is 8 < 5 or 8 > 6: {result2}")
+result3 = not True
+print(f"is reverse  True:,{result3}")
+num = 4
+result4 =  num > 0 and num % 2 == 0
+print(f"is 4 > 0 and 4 % 2 == 0:,{result4}")
+
+age = 18
+has_id = True
+print(f"is age == 18 and has id == True:,{age == 18 and has_id == True}")
+marks = 50
+attendance = 75
+print(f"is marks == 50 or attendance > 75:,{marks == 50 or  attendance > 75:}")
+my_list = []
+result7 = not my_list
+print(f"is list  empty:,{result7}")
+x= 5
+y=4
+result8 = x > 5 and y < 4
+print(f" is x > 5 and y < 4:,{result8}")
+a= 2
+b=4
+c=2
+print(f"is a == b or b == c:,{a == b or b == c}")
+is_student = False
+result10 =  not is_student
+print(f"is not astudent:,{result10}")
+
+
+
+
+
+
+      
+
+      
