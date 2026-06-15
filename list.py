6bf521661aaf7e67d70c07ebdf169e3680211883
pavithra <pavithraeraganaboyina@gmail.com>
2026-06-15 22:37:18 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,26 @@
+v=[ ]
+print(type (v)) 
+v=[1,3.4,"pavi","True",45,49,000]
+print(v[2])
+print(v[1:4:2])
+x=[1,2,3,4,5]
+x.append(6)
+print(x)
+z=[1,2,3,4,5]
+z.extend(["pavi",8,90,"False"])
+print(z)
+c=[1,2,3,4,5]
+c.remove(5)
+print(c)
+b=[1,2,3,4,5]
+b.pop(2)
+print(b)
+e=[1,2,3,4,5]
+print(e.count(4))
+
+s=[1,2,3,4,5]
+print(s.index(4))
+i=[1,2,3,4,5]
+i.insert(3,"pav")
+
+
